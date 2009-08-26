06ae8c17bf3733af919b859100bf9b2e6d589bc3
John Gunderman <johngunderman@gmail.com>
2009-08-26 12:58:19 -0400

    More prototyping before I start coding.

--- linked_lists.c
+++ linked_lists.c
@@ -18,8 +18,46 @@ int cmp_string_test () {
 }
 
 
+typedef struct {
+  void *key;
+  void *value;
+} list_entry_t;
+
+typedef struct {
+  list_entry_t *list
+  int (*cmp) (const void *, const void *); /* Comparison function */
+} list_head_t;
 
 
+/* 
+   Allocates a list head,
+   assigns the comparison function,
+   returns the list head.
+   new list should be empty
+   on error return NULL.
+   TODO
+ */
+list_head_t *list_init (int (*cmp) (const void *, const void *));
 
 
+/* 
+   Add a key and its corresponding value to the list.
+   Does NOT check whether the key is already present in the list.
+   Returns 0 on success, -1 on failure
+   TODO
+ */
+int list_insert (list_head_t *, void *key, void *value);
 
+
+/* 
+   search the list for an element whose key matches the second arg.
+   returns value of elem. or null if not found.
+   TODO
+ */
+void *list_search (list_head_t *, void *key);
+
+
+/* 
+   De-allocates the list head as well as all of the list entries.
+ */
+void list_delete (list_head_t *);
