8b9946a324d7fc8d873d5887644db81765e93be2
John Gunderman <johngunderman@gmail.com>
2009-09-07 20:38:04 -0400

    Filled out the search and insert functions.

--- hash_tables.h
+++ hash_tables.h
@@ -17,9 +17,24 @@
 typedef struct {
   size_t size;
   int (*cmp) (const void *, const void *);
+  int (*hash) (const void *);
   list_head_t **entries; 	/* entries is a dynamically
 				 sized array of linked lists */
 } hash_table_t;
+
+
+/* 
+   This function hashes strings for insertion into the hash table.
+ */
+int string_hasher (const void *key) {
+  int total = 0;
+  int i = 0;
+  while (((char *) key + i) != NULL){
+    total += *((char *)key + i);
+    i++;
+  }
+  return total;
+}
 
 
 
@@ -27,8 +42,9 @@ typedef struct {
    Allocates a new table with default size 100,
    assigns the comparison function,
    and returns an empty table. On error return NULL.
+   Takes a comparison function and a hashing function.
  */
-hash_table_t *hash_table_init (int (*cmp) (const void *, const void *)) {
+hash_table_t *hash_table_init (int (*cmp) (const void *, const void *), int (*hash) (const void *)) {
   hash_table_t *table = malloc (sizeof (hash_table_t));
   
   if (table == NULL) {
@@ -37,7 +53,7 @@ hash_table_t *hash_table_init (int (*cmp) (const void *, const void *)) {
   }
 
   table->cmp = cmp;
-  
+  table->hash = hash;
   table->entries =  calloc (TABLE_SIZE, sizeof (list_head_t*));
   table->size = TABLE_SIZE;
   
@@ -54,14 +70,33 @@ hash_table_t *hash_table_init (int (*cmp) (const void *, const void *)) {
    
    Returns 0 on success, -1 on failure.
 */
-int hash_table_insert (hash_table_t *table, void *key, void *value);
+int hash_table_insert (hash_table_t *table, void *key, void *value) {
+  int hash = table->hash (key) % table->size;
+  
+  list_entry_t *entry = malloc (sizeof (list_entry_t));
+  if (entry == NULL) {
+    fprintf (stderr, "Error on malloc in hash_table_insert");
+    return -1;
+  }
+
+  if (table->entries[hash] == NULL) {
+    table->entries[hash] = list_init (table->cmp);
+  }
+  list_insert (table->entries[hash], key, value);
+  return 0;
+}
 
 
 /* 
    Search the table for an element whose key matches the second arg.
    returns value of elem or NULL if not found.
 */
-void *hash_table_search (hash_table_t *table, void *key);
+void *hash_table_search (hash_table_t *table, void *key) {
+  int hash = table->hash (key) % table->size;
+  
+  if (table->entries[hash] == NULL) return NULL;
+  else return list_search (table->entries[hash], key);
+}
 
 
 
