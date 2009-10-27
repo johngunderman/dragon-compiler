966d2fbc1564ffa3a5362175e34a2d5ef2c5f73b
John Gunderman <johngunderman@gmail.com>
2009-10-27 13:21:51 -0400

    added framework for generating intermediate code.

---
+++ intmdt_code_gen.c
@@ -0,0 +1,7 @@
+/*  
+    By: John Gunderman
+    This file contains function definitions for generation
+    of intermediate code.
+*/
+
+
---
+++ intmdt_code_gen.h
@@ -0,0 +1,48 @@
+/* 
+   Header for intmdt_code_gen.c
+   By: John Gunderman
+   This file contains structs and function prototypes dealing with
+   the generation of intermediate code.
+*/
+
+
+#define MAXCODELEN 8192
+
+
+typedef struct {
+  enum {symbol, int_const, float_const, bool_const, code} type;
+  union {
+    list_entry_t *entry_ptr;
+    int *int_const_ptr;
+    double *double_const_ptr;
+    int *bool_const_ptr;
+    quadruple_t *instr_ptr;
+  } addr;
+} intmdt_addr_t;
+
+
+typedef struct quadruple quadruple_t;
+struct quadruple {
+  char *op;
+  intmdt_addr_t *arg1;
+  intmdt_addr_t *arg2;
+  intmdt_addr_t *result;
+};
+
+
+
+typedef struct {
+  quadruple_t code[MAXCODELEN];
+  unsigned int n; /* Number of instructions in code */
+} intmdt_code_t;
+
+
+int gen(intmdt_code_t intermediate_code,
+        char *op, intmdt_addr_t *arg1, intmdt_addr_t *arg2,
+        intmdt_addr_t *result);
+
+
+intmdt_addr_t *newtemp(env_t *top);
+
+list_entry_t *widen(intmdt_addr_t *a, id_type_t *t);
+
